b5debdf9ca4e530c4b195a0874e3dc5a8a29c615
rpadalka <moxnoryl@tut.by>
2016-07-24 22:52:37 +0300

    Add new annotation

---
+++ src/main/java/matrix/annotation/Nimble.java
@@ -0,0 +1,11 @@
+package matrix.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by rpadalka on 24.07.16.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Nimble {
+}
